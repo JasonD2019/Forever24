f73c967d850ceecf61af07406cd87174eafe2e76
Jason Ding <xingjian.ding98@gmail.com>
2021-11-04 17:37:03 -0500

    Initial Commit

---
+++ 24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  24
+//
+//  Created by 丁行健 on 11/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
