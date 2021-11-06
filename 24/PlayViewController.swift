e7ecbfcaeaf5218887bbe5f74c09557b261cf265
Jason Ding <Xingjian.ding98@gmail.com>
2021-11-06 00:43:15 -0400

    1

---
+++ 24/PlayViewController.swift
@@ -0,0 +1,117 @@
+//
+//  PlayViewController.swift
+//  24
+//
+//  Created by 丁行健 on 11/5/21.
+// Original cards images are from Playing Cards iOS Assets https://github.com/hayeah/playing-cards-assets.
+
+import UIKit
+
+class PlayViewController: UIViewController {
+
+    @IBOutlet var plus: UIButton!
+    @IBOutlet var minus: UIButton!
+    @IBOutlet var times: UIButton!
+    @IBOutlet var over: UIButton!
+    @IBOutlet var textField: UILabel!
+    @IBOutlet var l_parenthesis: UIButton!
+    @IBOutlet var r_parenthesis: UIButton!
+    @IBOutlet var card1: UIImageView!
+    @IBOutlet var card2: UIImageView!
+    @IBOutlet var card3: UIImageView!
+    @IBOutlet var card4: UIImageView!
+    var card_1_Name: String = ""
+    var card_2_Name: String = ""
+    var card_3_Name: String = ""
+    var card_4_Name: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presentCards()
+        clickCheck()
+        // Do any additional setup after loading the view.
+    }
+    
+    func presentCards(){
+        card_1_Name = "2_clubs"
+        let card_1_image = UIImage(named: card_1_Name)
+        card1.image = card_1_image
+        
+        card_2_Name = "2_diamonds"
+        let card_2_image = UIImage(named: card_2_Name)
+        card2.image = card_2_image
+        
+        card_3_Name = "8_spades"
+        let card_3_image = UIImage(named: card_3_Name)
+        card3.image = card_3_image
+        
+        card_4_Name = "3_hearts"
+        let card_4_image = UIImage(named: card_4_Name)
+        card4.image = card_4_image
+    }
+    
+    func clickCheck(){
+        let gesture1 = UITapGestureRecognizer(target: self, action:  #selector(self.clickCard1(sender:)))
+        let gesture2 = UITapGestureRecognizer(target: self, action:  #selector(self.clickCard2(sender:)))
+        let gesture3 = UITapGestureRecognizer(target: self, action:  #selector(self.clickCard3(sender:)))
+        let gesture4 = UITapGestureRecognizer(target: self, action:  #selector(self.clickCard4(sender:)))
+        card1.addGestureRecognizer(gesture1)
+        card2.addGestureRecognizer(gesture2)
+        card3.addGestureRecognizer(gesture3)
+        card4.addGestureRecognizer(gesture4)
+    }
+    
+    @objc func clickCard1(sender : UITapGestureRecognizer) {
+        let name = card_1_Name
+        print(name)
+    }
+
+    @objc func clickCard2(sender : UITapGestureRecognizer) {
+        let name = card_2_Name
+        print(name)
+    }
+    
+    @objc func clickCard3(sender : UITapGestureRecognizer) {
+        let name = card_3_Name
+        print(name)
+    }
+    
+    @objc func clickCard4(sender : UITapGestureRecognizer) {
+        let name = card_4_Name
+        print(name)
+    }
+    
+    @IBAction func clickPlus(_ sender: Any) {
+        textField.text = "\(textField.text!) +"
+    }
+    @IBAction func clickMinus(_ sender: Any) {
+        textField.text = "\(textField.text!) -"
+    }
+    @IBAction func clickTimes(_ sender: Any) {
+        textField.text = "\(textField.text!) x"
+    }
+    @IBAction func clickOver(_ sender: Any) {
+        textField.text = "\(textField.text!) %"
+    }
+    @IBAction func clickLP(_ sender: Any) {
+        textField.text = "\(textField.text!) ("
+    }
+    @IBAction func clickRP(_ sender: Any) {
+        textField.text = "\(textField.text!) )"
+    }
+    @IBAction func clickClear(_ sender: Any) {
+        textField.text = ""
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 24/ViewController.swift
+++ 24/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var playButton: UIButton!
+    @IBOutlet var settingButton: UIButton!
+    @IBOutlet var aboutButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setButtonstyle()
     }
 
-
+    func setButtonstyle(){
+        playButton.backgroundColor = .clear
+        playButton.layer.cornerRadius = 5
+        playButton.layer.borderWidth = 1
+        playButton.layer.borderColor = UIColor.black.cgColor
+        settingButton.backgroundColor = .clear
+        settingButton.layer.cornerRadius = 5
+        settingButton.layer.borderWidth = 1
+        settingButton.layer.borderColor = UIColor.black.cgColor
+        aboutButton.backgroundColor = .clear
+        aboutButton.layer.cornerRadius = 5
+        aboutButton.layer.borderWidth = 1
+        aboutButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    @IBAction func goToGamePage(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "gameVC") as! PlayViewController
+        present(vc, animated: true)
+    }
+    
+    @IBAction func goToSettingPage(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "settingVC") as! SettingViewController
+        present(vc, animated: true)
+    }
+    
+    @IBAction func goToAboutPage(_ sender: Any) {
+        let vc = storyboard?.instantiateViewController(identifier: "aboutVC") as! AboutViewController
+        present(vc, animated: true)
+    }
 }
 
